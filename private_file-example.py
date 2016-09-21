a3a78f714324aac11e45eee0b66d00b07a6a9709
Bas Nijholt <basnijholt@gmail.com>
2016-09-21 14:16:36 +0200

    Add comment in private_file

--- private_file-example.py
+++ private_file-example.py
@@ -2,5 +2,5 @@ import datetime
 
 my_mail = "my_email@gmail.com"
 her_mail = "her_email@gmail.com"
-start_date = datetime.date(2016, 9, 21)
+start_date = datetime.date(2016, 9, 21)  # date at which you start using the script
 day_one_text_file = "colombia.txt"  #"india.txt"
