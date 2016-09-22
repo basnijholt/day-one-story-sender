e19c20b7d43149ef15ed51fd9e6c33135bbffb47
Bas Nijholt <basnijholt@gmail.com>
2016-09-22 14:27:57 +0200

    Update .gitignore

--- private_variables_example.py
+++ private_variables_example.py
@@ -3,4 +3,4 @@ import datetime
 my_mail = "my_email@gmail.com"
 her_mail = "her_email@gmail.com"
 start_date = datetime.date(2016, 9, 21)  # date at which you start using the script
-day_one_file = "india.json"  #"colombia.json"
+day_one_file = "india.json"  # "colombia.json"s
